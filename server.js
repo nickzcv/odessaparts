b3c2018f6c157532bf0e531b4c8e7e1ee3144890
Nick Zaicev <Mikalai.Zaitsau1@ibm.com>
2021-01-17 11:18:23 +0300

    upd server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.use('/', express.static('./cominsoon/'));
+app.use('/', express.static('cominsoon'));
 app.set('port', port);
 app.listen(app.get('port') || port, function() {
   console.log('Site started!');
