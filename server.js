5e5572935efda528b8141366e190a224fd9f9d27
Nick Zaicev <Mikalai.Zaitsau1@ibm.com>
2021-01-17 11:46:38 +0300

    add morgan for debug

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan  = require('morgan')
 const app = express();
+
 const port = 80;
 
+app.use(morgan('combined'));
 app.use('/', express.static('./'));
 app.set('port', port);
 app.listen(app.get('port') || port, function() {
