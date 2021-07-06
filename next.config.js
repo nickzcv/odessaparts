0d7b0a505de89ca74b5139cb65ce955c6e91f96a
Zaitsev Nick <Mikalai.Zaitsau1@ibm.com>
2021-07-06 09:42:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 // noinspection JSUnusedGlobalSymbols
 module.exports = {
-    future: {
-        webpack5: true,
-    },
-    env: {
-        basePath: process.env.BASE_PATH || '',
-    },
+  future: {
+    webpack5: true,
+  },
+  env: {
     basePath: process.env.BASE_PATH || '',
-    i18n: {
-        locales: ['en', 'ru', 'ar'],
-        defaultLocale: 'en',
-    },
+  },
+  basePath: process.env.BASE_PATH || '',
+  i18n: {
+    locales: ['en', 'ru', 'ar'],
+    defaultLocale: 'en',
+  },
 };
